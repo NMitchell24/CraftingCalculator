1bcb6db9e6c6a2853cbc508aa4b83405c1ee9a95
hignz <higz-06@hotmail.com>
2019-03-24 16:54:41 +0000

    added reset preferences menu item

--- CraftingCalculator/ViewModel/CraftingCalculatorMainViewModel.cs
+++ CraftingCalculator/ViewModel/CraftingCalculatorMainViewModel.cs
@@ -12,6 +12,7 @@ namespace CraftingCalculator.ViewModel
         public CommandRunner TopMostCommand { get; set; }
         public CommandRunner ChangeThemeCommand { get; set; }
         public CommandRunner ChangeAccentCommand { get; set; }
+        public CommandRunner ResetSettingsCommand { get; private set; }
 
         private bool _isTopMost;
         public bool IsTopMost
@@ -29,10 +30,24 @@ namespace CraftingCalculator.ViewModel
             TopMostCommand = new CommandRunner(ToggleTopMost);
             ChangeThemeCommand = new CommandRunner(ChangeTheme);
             ChangeAccentCommand = new CommandRunner(ChangeAccent);
+            ResetSettingsCommand = new CommandRunner(ResetSettings);
 
             ChangeTheme(Properties.Settings.Default["Theme"]);
         }
 
+        /// <summary>
+        /// Resets Properties.Settings
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ResetSettings(object obj)
+        {
+            Properties.Settings.Default.Reset();
+        }
+
+        /// <summary>
+        /// Changes application theme while preserving current accent, updates theme in Properties.Settings
+        /// </summary>
+        /// <param name="obj"></param>
         private void ChangeTheme(object obj)
         {
             string accent = Properties.Settings.Default["Accent"].ToString();
@@ -44,6 +59,10 @@ namespace CraftingCalculator.ViewModel
             Properties.Settings.Default["Theme"] = obj;
         }
 
+        /// <summary>
+        /// Changes application accent while preserving current theme, updates accent in Properties.Settings
+        /// </summary>
+        /// <param name="obj"></param>
         private void ChangeAccent(object obj)
         {
             string theme = $"Base{Properties.Settings.Default["Theme"]}";
@@ -55,11 +74,20 @@ namespace CraftingCalculator.ViewModel
             Properties.Settings.Default["Accent"] = obj;
         }
 
+        /// <summary>
+        /// Inverts IsTopMost bool
+        /// </summary>
+        /// <param name="obj"></param>
         private void ToggleTopMost(object obj)
         {
             IsTopMost = !IsTopMost;
         }
 
+        /// <summary>
+        /// Is called when window is closed, provides confirmation dialog and saves Properties.Settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void OnWindowClosing(object sender, CancelEventArgs e)
         {
             //var result = MessageBox.Show("Are you sure you want to quit?", "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question);
