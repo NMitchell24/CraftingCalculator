07d4ddda2dd87bc27ea63063188c856ec8516520
hignz <higz-06@hotmail.com>
2019-03-24 14:14:17 +0000

    added Theme and Accent strings to Properties.Settings

--- CraftingCalculator/Properties/Settings.Designer.cs
+++ CraftingCalculator/Properties/Settings.Designer.cs
@@ -70,5 +70,29 @@ namespace CraftingCalculator.Properties {
                 this["Left"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Light")]
+        public string Theme {
+            get {
+                return ((string)(this["Theme"]));
+            }
+            set {
+                this["Theme"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Blue")]
+        public string Accent {
+            get {
+                return ((string)(this["Accent"]));
+            }
+            set {
+                this["Accent"] = value;
+            }
+        }
     }
 }
