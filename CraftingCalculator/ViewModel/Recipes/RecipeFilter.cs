203cf69c1f1ab6fe76e3287f3352ee369dbcf634
Nathan Mitchell <Natey24@gmail.com>
2020-11-01 21:07:37 -0600

    Add text search to large data lists.

--- CraftingCalculator/ViewModel/Recipes/RecipeFilter.cs
+++ CraftingCalculator/ViewModel/Recipes/RecipeFilter.cs
@@ -10,7 +10,7 @@ namespace CraftingCalculator.ViewModel.Recipes
         public string Name { get; set; }
         public int Id { get; set; }
         public string Description { get; set; }
-        public string Tooltip { get; set; }
+        public string Tooltip { get => Description; set { } }
         public DataType Type
         {
             get
